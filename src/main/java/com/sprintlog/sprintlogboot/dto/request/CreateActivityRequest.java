f305a9ca56c2edb4b9bd2f0189cc1cad68829db9
hisjeans <his7jeans@gmail.com>
2026-06-12 13:24:06 +0900

    DTO와 JSON 필드 이름 불일치를 @JsonProperty로 해결

--- src/main/java/com/sprintlog/sprintlogboot/dto/request/CreateActivityRequest.java
+++ src/main/java/com/sprintlog/sprintlogboot/dto/request/CreateActivityRequest.java
@@ -1,5 +1,6 @@
 package com.sprintlog.sprintlogboot.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sprintlog.sprintlogboot.domain.ActivityCategory;
 import com.sprintlog.sprintlogboot.domain.Visibility;
 import jakarta.validation.constraints.Max;
@@ -13,6 +14,10 @@ public record CreateActivityRequest(
         // 빈 문자열, 공백 문자열 허용 대신 null은 안 됨!
         // 필수값으로 지정하기 위해
         @NotNull(message = "활동 유형(type)은 필수입니다.")
+        @JsonProperty("category") // 왠만하면 프론트와 백엔드 맞춰주는 것이 좋지만,
+        // 백엔드는 'type'이란 이름으로 올 것을 기대하고 있고 프론트는 'category' 란 이름을 기대하는 불일치 문제 발생
+        // 단순 변경 시에는 다른 코드까지 변경되어야 하기 때문에 
+        // @JsonProperty 어노테이션 이용해 type-category 짝임을 알려준다
         ActivityCategory type,
 
         // @NotEmpty: 공백 문자열 허용, 빈 문자열, null은 안 됨!
