9c6a7eb4fc3a75bdb95fb5a2ba24d9bc45878c4f
hisjeans <his7jeans@gmail.com>
2026-07-29 08:12:23 +0900

    Spring Data JPA 구현하기 - CascadeType.ALL과 고아 객체(orphanRemoval)에 대한 내용 확인

--- src/main/java/com/sprintlog/sprintlogboot/domain/User.java
+++ src/main/java/com/sprintlog/sprintlogboot/domain/User.java
@@ -1,6 +1,7 @@
 package com.sprintlog.sprintlogboot.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
@@ -30,9 +31,36 @@ public class User extends BaseEntity {
   // 연관관계의 주인이 아는 User는 mappedBy 속성을 세팅해준다
   // "owner" 값을 세팅하는 건, 연관관계의 주인 쪽 필드가 User다 라는 것을 얘기해 주는 것
   // 여기에 선언된 활동 리스트는 실제 DB에는 존재하지 않는 데이터, 이건 JPA가 연관관계를 보고 만들어주는 가상의 컬럼
-  @OneToMany(mappedBy = "owner") // 연관관계의 진짜 주인은 LearningActivity의 owner가 진짜 주인
+  // 부모가 자식의 생명주기를 100% 제어하고 싶을 때 cascade와 orphanRemoval을 같이 걸어주는 경우가 흔하다
+  @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true) // 연관관계의 진짜 주인은 LearningActivity의 owner가 진짜 주인, 고아 객체는 삭제하는 것이 true
   @JsonIgnore
   private List<LearningActivity> activities=new ArrayList<>();
+
+      /*
+        # CascadeType.PERSIST
+        엔터티를 영속화할 때, 연관된 하위 엔터티도 함께 영속화 한다.
+        연관 관계의 주인은 Employee. Department의 list는 가상 데이터.
+        주인이 아닌 부모가 자식의 생명 주기에 영향을 주고 싶을 때 사용. (저장)
+
+        ## CascadeType.ALL
+        모든 Cascade를 적용한다.
+        거의 대부분 CascadeType 쓴다면 ALL 한다
+
+        # CascadeType.MERGE
+        엔티티 상태를 병합(Merge)할 때, 연관된 하위 엔티티도 모두 병합한다.
+        부모를 수정해서 반영하면 그 안에 있는 자식도 함께 반영됩니다.
+
+        # CascadeType.REMOVE
+        엔티티를 제거할 때, 연관된 하위 엔티티도 모두 제거한다.
+
+        CascadeType.DETACH
+        영속성 컨텍스트에서 엔티티 제거
+        부모 엔티티를 detach()(Entity Manager) 수행하면, 연관 하위 엔티티도 detach()상태가 되어 변경 사항을 반영하지 않는다.
+
+        CascadeType.REFRESH (Entity Manager)
+        상위 엔티티를 새로고침(Refresh)할 때, 연관된 하위 엔티티도 모두 새로고침한다.
+
+     */
 
   // user, learningActivity, member, book, loan, author..는 pk->id, createdAt, updatedAt 공통적으로 가질 가능성 높다
 
